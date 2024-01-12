56b128653e4110cfa4698d7703dff6681012fd62
AnkushSarkar10 <ankush.sarkar2002@gmail.com>
2024-01-12 15:08:01 -0500

    sanity setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        mporange: "#F93304",
+        mporange: "#f93304",
+        mpblue: "#0041db",
+        mpgray: "#c1c3c0",
+        mpblack: "#0c0c0c",
       },
     },
   },
